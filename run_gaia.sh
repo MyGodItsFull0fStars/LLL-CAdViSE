b82ed4c84eb9bd12957a5bcb6268a18258795499
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-06-01 12:44:45 +0200

    Added trapCleanup function

--- run_gaia.sh
+++ run_gaia.sh
@@ -44,9 +44,15 @@ cleanExit() {
   rm -rf "$id"
   exit "$1"
 }
+trapCleanup() {
+  showMessage "Killing EC2 instances and clean ups"
+  aws ec2 terminate-instances --instance-ids $clientInstanceIds $serverInstanceId --profile $awsProfile &>/dev/null
+  rm -rf "$id"
+  exit 1
+}
 
 # Register cleanExit function to execute on SIGINT
-trap cleanExit SIGINT
+trap trapCleanup SIGINT
 
 showDebugMessage "Debug Mode ON"
 
